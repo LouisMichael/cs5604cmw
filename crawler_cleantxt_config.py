ccec0608fb90bed8e7bba77472d1a4c241c4128f
xlxlxlx <32310857+xlxlxlx@users.noreply.github.com>
2017-11-06 19:10:15 -0500

    Update crawler_cleantxt_config.py

--- crawler_cleantxt_config.py
+++ crawler_cleantxt_config.py
@@ -6,3 +6,14 @@ COL_ID = {
 }
 
 COL_EVENT = "Eclipse2017"
+
+OUTPATH = "output/"
+
+OUTFN = "cmwf17test.tsv"
+
+## This is more like a manual. Not really used in our code. 
+REF_CODE = {
+"1000":"Content-Type is not text/html",
+"2000":"No text data",
+"2001":"Unknown reason (URL content successfully fetched)",
+}
