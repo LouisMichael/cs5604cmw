f941d746386b7e6be1b7f1b3afa2c3a14cccfebe
Xiao Liang <xlianguw@uw.edu>
2017-12-10 17:06:54 -0500

    change the directory

--- FinalSubmission/crawler_cleantxt_config_twitter.py
+++
@@ -1,9 +0,0 @@
-OUTPATH = "output/"
-OUTFN = "cmwf17twitter.tsv"
-
-## This is more like a manual. Not really used in our code. 
-REF_CODE = {
-"1001":"Content-Type is not text/html",
-"2000":"No text data",
-"2001":"Unknown reason (URL content successfully fetched)",
-}
